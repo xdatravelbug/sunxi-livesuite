faf9c412311b20b71693b40443b4f6488f671d53
xdatravelbug <xdatravelbug@users.noreply.github.com>
2021-02-04 21:47:18 +0100

    Adapt to Linux Kernels >= 4.11
    since Linux 4.11 the header file that declares the signal_pending function starting from <linux/sched.h> became <linux/sched/signal.h>

--- awusb/awusb.c
+++ awusb/awusb.c
@@ -27,7 +27,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/signal.h>
-#include <linux/sched.h>
+#include <linux/sched/signal.h>
 #include <linux/hardirq.h>     //<linux/smp_lock.h>
 #include <linux/errno.h>
 #include <linux/random.h>
